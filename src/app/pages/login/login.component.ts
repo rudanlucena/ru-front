a1d41662173fbebf5f7bd6a9f1cef8ff6160ca9e
alexalins <alexa.glinsoo@gmail.com>
2019-06-11 17:22:42 -0300

    linha de separação no inicio

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Login } from 'src/app/models/Login';
 import { LoginService } from 'src/app/services/login.service';
 import { Token } from 'src/app/models/Token';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +14,22 @@ export class LoginComponent implements OnInit {
   public login: Login = new Login();
   public token: Token = new Token();
 
-  constructor(private loginService: LoginService) { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
- loginUsuario(){
-    console.log(this.login);
+  //metodo de login
+  loginUsuario() {
     try {
-      this.loginService.login(this.login).subscribe( res => {
+      this.loginService.login(this.login).subscribe(res => {
         this.token.Authorization = res.body.Authorization;
+        //salvando token na sessao
         sessionStorage.setItem("token", this.token.Authorization);
+        this.router.navigate(['/pages/inicio']);
       });
     } catch (e) {
       console.log(e);
