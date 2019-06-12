ba5a739e7c60e18d64eeea60ccd563536ac599d9
alexalins <alexa.glinsoo@gmail.com>
2019-06-12 08:54:13 -0300

    corrigindo o header http

--- src/app/services/autor.service.ts
+++ src/app/services/autor.service.ts
@@ -4,22 +4,25 @@ import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Autor } from '../models/autor';
 
+const httpOptions = new HttpHeaders(
+  {
+    'Content-Type': 'application/json',
+    'Authorization': sessionStorage.getItem('token')
+  }
+);
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AutorService {
-  
-  private url = environment.host+'api/v1/autores';
-  constructor(private http: HttpClient) { }
 
-  salvar(autor : Autor) : Observable<HttpResponse<Autor>>{
-    return this.http.post<Autor>(this.url, autor, {
-      headers : {
-        'Content-Type': 'application/json',
-        'Authorization': sessionStorage.getItem('token')
-      },
-      observe : 'response'});
+  private url = environment.host + 'api/v1/autores';
+
+  constructor(private http: HttpClient) {
+  }
+
+  salvar(autor: Autor): Observable<HttpResponse<Autor>> {
+    return this.http.post<Autor>(this.url, autor, { headers: httpOptions, observe: 'response' });
   }
 }
